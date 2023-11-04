88fd2b9f415b8fdf9c27b62afd4f8885002354d1
IvanBevza <ivan@trainual.com>
2023-11-04 17:53:08 +0200

    add validation for title and body

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,6 @@ class Subject < ApplicationRecord
   has_one_attached :body_audio
   has_one_attached :intro_audio
   has_one_attached :summary_audio
+
+  validates :title, :body, presence: true
 end
