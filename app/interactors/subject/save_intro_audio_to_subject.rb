eb40cf035d2fb8ef3feef982151d42e526b06bda
OlesStankevych <oles@trainual.com>
2023-11-05 10:27:49 +0200

    Create Interactors for Intro Generation and Management
    Introduced various interactors to handle intro generation, saving the intro, and its transcription for a given subject. These interactors will help to generate and save intro text, convert it to audio, get its transcription and manage this flow. Additionally, integrated the intro generation process into summary generation for a complete lifecycle management

---
+++ app/interactors/subject/save_intro_audio_to_subject.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Subject::SaveIntroAudioToSubjectContext < ActiveInteractor::Context::Base
+  attributes :subject, :audio, :audio_type
+
+  validates :subject, :audio, :audio_type, presence: true, on: :calling
+end
+
+class Subject::SaveIntroAudioToSubject < ActiveInteractor::Base
+  def perform
+    attach_audio_to_subject
+
+    unless context[:subject].save
+      context.errors.add(:base, 'Failed to save audio to subject')
+      context.fail!(context[:subject].errors)
+    end
+  end
+
+  private
+
+  def attach_audio_to_subject
+    context[:subject].intro_audio.attach(
+      io: StringIO.new(context.audio),
+      filename:,
+      content_type: 'audio/mpeg'
+    )
+  end
+
+  def filename
+    "#{context[:subject].id}_#{Time.now.to_i}.#{context.audio_type}"
+  end
+end
